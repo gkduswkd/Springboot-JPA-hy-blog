9ad0c5a299bd832213a4bb284fac524399e2ecd3
gkduswkd <kuramotojin@naver.com>
2020-11-10 12:27:16 +0900

    환경세팅 완료

---
+++ src/main/java/com/hy/blog/HyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hy/blog/HyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
